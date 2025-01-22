0b73093c7fb6732f497aa1cf3b853b4aaeea48f6
24bcp395 <24bcp395@sot.pdpu.ac.in>
2025-01-22 23:43:17 +0530

    Add files via upload

---
+++ problem 11(2).py
@@ -0,0 +1,11 @@
+def onstraight():
+    a,b=input("enter two number here:").split()
+    print("x1="a,"y1="b)
+    c,d=input("enter two number here:").split()
+    print("x2="a,"y2="b)
+    e,f=input("enter two number here:").split()
+    print("x3="a,"y3="b)
+
+    if((a,b)==(c,d)==(e,f)):
+        print("all points on line")
+onstraight()
